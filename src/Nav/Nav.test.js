a34db74b879abe2368eeddb22b705a51eea2c1bb
Tito anguiano <njanguiano@gmail.com>
2020-10-26 12:13:10 -0700

    refactoring

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Nav from './Nav';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Nav />, div);
+import Nav from "./Nav";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <Nav />
+    </BrowserRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
